48b25b73230e2bd1f9d59373db0ae88c6eb9fb67
Mark O'Neil <zeretic@gmail.com>
2023-05-17 12:13:16 -0400

    update config and learnapisintro.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,8 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://probable-broccoli-vr1gken.pages.github.io',
+  // url: 'https://probable-broccoli-vr1gken.pages.github.io',
+  url: 'https://blackboard.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/',
