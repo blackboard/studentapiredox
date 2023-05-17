850e08528fcdcbb49ad47568fc359b9e9c9ae0bf
Mark O'Neil <zeretic@gmail.com>
2023-05-17 10:14:06 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,8 +14,8 @@ const config = {
   url: 'https://probable-broccoli-vr1gken.pages.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
+  // baseUrl: '/',
+  baseUrl: '/studentapiredox/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Anthology', // Usually your GitHub org/user name.
