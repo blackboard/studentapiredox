195869e84d85384684df35df2b8cb10464fddb23
Mark O'Neil <zeretic@gmail.com>
2023-05-22 12:06:07 -0400

    disable rendering of academics.json

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -101,11 +101,11 @@ const config = {
           //   spec: 'openapi/learn/learn-swagger-3900.62.0.json',
           //   route: '/openapi/learn/learn-swagger-3900.62.0',
           // },
-          {
-            id:'studentacademics',
-            spec: 'openapi/student/academics.json',
-            route: '/openapi/student/academics',
-          },
+          // {
+          //   id:'studentacademics',
+          //   spec: 'openapi/student/academics.json',
+          //   route: '/openapi/student/academics',
+          // },
           {
             id:'studentadmissions',
             spec: 'openapi/student/admissions.json',
